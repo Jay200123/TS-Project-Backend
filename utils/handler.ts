95eb91f25ccb0ef472a73c7e7e45ec68295d5755
Jay200123 <kenken.ayonon@gmail.com>
2024-11-13 18:01:16 +0800

    Add ErrorHandler and SuccessHandler for standardized response handling

---
+++ utils/handler.ts
@@ -0,0 +1,37 @@
+import { STATUSCODE } from "../constants";
+import { Response } from '../interface';
+
+class ErrorHandler extends Error {
+    public statusCode: number;
+
+    constructor(message: string, statusCode: number = STATUSCODE.BAD_REQUEST) {
+        super(message);
+        this.statusCode = statusCode;
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+
+    toJSON() {
+        return {
+            success: false,
+            error: {
+                message: this.message,
+            },
+        };
+    }
+}
+
+const SuccessHandler = (res: Response, message: string, details: any, token?: string) => {
+    res.status(STATUSCODE.SUCCESS).json({
+        success: true,
+        message: message,
+        details: details,
+        ...(token && { access: token }),
+    });
+};
+
+
+export {
+    ErrorHandler,
+    SuccessHandler
+} 
