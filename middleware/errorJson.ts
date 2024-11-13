78dbedeebf9a2902876ef75dbbbda56ff3972acc
Jay200123 <kenken.ayonon@gmail.com>
2024-11-13 23:04:57 +0800

    Add error handling middleware for JSON responses

---
+++ middleware/errorJson.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "../interface";
+import { ErrorHandler } from "../utils";
+import { STATUSCODE, ERROR } from "../constants";
+import { ErrorMiddleware } from "../interface";
+
+const errorJson: ErrorMiddleware = (err, req, res, next) => {
+    if (!(err instanceof ErrorHandler)) {
+        err = new ErrorHandler(err.message || ERROR.INTERNAL_SERVER_ERROR);
+    }
+    next(err);
+};
+
+const errorHandler: ErrorMiddleware = (err, req, res, next) => {
+    const statusCode = err.statusCode || STATUSCODE.INTERNAL_SERVER_ERROR;
+    const message = err.message || ERROR.INTERNAL_SERVER_ERROR;
+
+    res.status(statusCode).json({
+        success: false,
+        error: {
+            message: message,
+        },
+    });
+};
+
+export { errorJson, errorHandler };
