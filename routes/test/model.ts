a1b8ad47ec557cf7c953ed5511948c5b73c558d1
Jay200123 <kenken.ayonon@gmail.com>
2024-11-13 15:51:26 +0800

    Add TestSchema model

---
+++ routes/test/model.ts
@@ -0,0 +1,18 @@
+import { Schema, model, ITest } from "../../interface";
+import { RESOURCE } from "../../constants"
+
+const TestSchema: Schema<ITest> = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    image: [
+        {
+            public_id: String,
+            url: String,
+            originalname: String
+        }
+    ]
+});
+
+const Test = model<ITest>(RESOURCE.TESTS, TestSchema);
