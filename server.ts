c8435076f09c57a4a77cd42e1324ff95008471a5
Jay200123 <kenken.ayonon@gmail.com>
2024-11-13 23:04:31 +0800

    Set up Express server with middleware, routes, and error handling

---
+++ server.ts
@@ -0,0 +1,40 @@
+import { express, mongoose } from "./interface";
+import { globalEnvironment, connectDB } from "./config";
+import { test } from "./routes";
+import { upload } from "./utils";
+import { errorJson, errorHandler } from "./middleware";
+import path from "path";
+
+globalEnvironment();
+connectDB();
+const app = express();
+
+app.use(upload.array('image'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+})
+
+app.use("/api/v1", test);
+
+app.all("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "notFound.html"));
+})
+
+app.use(errorJson);
+app.use(errorHandler);
+
+mongoose.connection.once('open', () => {
+    app.listen(process.env.PORT);
+    console.log(`Server running on ${process.env.PORT}`);
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log(err);
+    process.exit(1);
+})
+
