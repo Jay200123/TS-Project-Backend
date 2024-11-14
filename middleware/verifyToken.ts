53ab72f37a375a1825adbbde449ae212d35a2c54
Jay200123 <kenken.ayonon@gmail.com>
2024-11-14 17:13:14 +0800

    Add verifyToken middleware for JWT authentication and user validation

---
+++ middleware/verifyToken.ts
@@ -0,0 +1,33 @@
+
+import {
+    Response,
+    NextFunction,
+    DecodeToken,
+    AuthenticatedRequest
+} from ".././interface";
+import userService from "../routes/user/service";
+import { ErrorHandler } from "../utils";
+import { jwt } from "../interface";
+import { findBlacklist } from "./blacklist";
+
+const verifyToken = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    try {
+        if (!req.headers['authorization'] || !req.headers['authorization'].startsWith('Bearer ')) {
+            return next(new ErrorHandler("Authorization header missing"));
+        }
+
+        if (findBlacklist(req.headers['authorization'].split(' ')[1])) {
+            return next(new ErrorHandler("User must login first"));
+        }
+
+        const decode = jwt.verify(req.headers['authorization'].split(' ')[1], process.env.JWT_SECRET) as DecodeToken;
+        req.user = await userService.getById(decode._id);
+        next();
+    } catch (err) {
+        return next(new ErrorHandler("User must login first"));
+    }
+}
+
+export {
+    verifyToken,
+}
