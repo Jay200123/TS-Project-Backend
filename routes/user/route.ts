bd55e8cbdc85442a445b5c596125f6c32b2e1b5b
Jay200123 <kenken.ayonon@gmail.com>
2024-11-14 20:46:33 +0800

    Add role-based access control and token verification to user routes

--- routes/user/route.ts
+++ routes/user/route.ts
@@ -1,6 +1,7 @@
 import * as userController from './controller';
 import { Router } from "./../../interface";
-import { METHOD, PATH, } from '../../constants';
+import { METHOD, PATH, ROLE } from '../../constants';
+import { verifyToken, userRole } from '../../middleware';
 
 const router = Router();
 
@@ -8,28 +9,36 @@ const userRouter = [
     {
         method: METHOD.GET,
         path: PATH.USERS,
+        middleware: [verifyToken],
+        role: [ROLE.ADMIN, ROLE.CUSTOMER],
         handler: userController.getAllUser
     },
     {
         method: METHOD.GET,
         path: PATH.USER_ID,
+        middleware: [verifyToken],
+        role: [ROLE.ADMIN, ROLE.CUSTOMER],
         handler: userController.getUserById
     },
     {
         method: METHOD.PATCH,
         path: PATH.EDIT_USER_ID,
+        middleware: [verifyToken],
+        role: [ROLE.ADMIN, ROLE.CUSTOMER],
         handler: userController.updateUserById
     },
     {
         method: METHOD.DELETE,
         path: PATH.USER_ID,
+        middleware: [verifyToken],
+        role: [ROLE.ADMIN],
         handler: userController.deleteUserById
     }
 ];
 
 userRouter.forEach((route) => {
-    const { method, path, handler } = route;
-    router[method as any](path, handler);
+    const { method, path, middleware = [], role = [], handler } = route;
+    router[method as any](path, middleware.concat(userRole(...role)), handler);
 });
 
 export default router;  
