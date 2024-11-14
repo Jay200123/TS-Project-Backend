ad338a3f8543c27d886a595acf4c37c3b6b8b2f3
Jay200123 <kenken.ayonon@gmail.com>
2024-11-14 10:47:03 +0800

    Update path constants to reflect new user and test routes

--- constants/path.ts
+++ constants/path.ts
@@ -1,5 +1,8 @@
 export const PATH = {
     TESTS: "/tests",
-    TEST_ID: "/tests/:id",
-    EDIT_TEST_ID: "/tests/edit/:id",
+    TEST_ID: "/test/:id",
+    EDIT_TEST_ID: "/test/edit/:id",
+    USERS: "/users",
+    USER_ID: "/user/:id",
+    EDIT_USER_ID: "/user/edit/:id",
 }
