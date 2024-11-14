d21a57105a9ea97107ac21cbfe80f8632ca0c3fc
Jay200123 <kenken.ayonon@gmail.com>
2024-11-14 10:46:11 +0800

    Add user service module with CRUD operations

---
+++ routes/user/service.ts
@@ -0,0 +1,30 @@
+import { IUser } from "../../interface";
+import User from "./model";
+
+const getAll = async () => {
+    return await User.find();
+};
+
+const getById = async (id: string) => {
+    return await User.findById(id);
+};
+
+const Add = async (data: IUser) => {
+    return await User.create(data);
+};
+
+const updateById = async (id: string, data: IUser) => {
+    return await User.findByIdAndUpdate(id, data, { new: true, runValidators: true });
+}
+
+const deleteById = async (id: string) => {
+    return await User.findByIdAndDelete(id);
+}
+
+export default {
+    getAll,
+    getById,
+    Add,
+    updateById,
+    deleteById
+}
