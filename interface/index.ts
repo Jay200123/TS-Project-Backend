c00b71bc1c896fb521419310d021265c596ea67b
Jay200123 <kenken.ayonon@gmail.com>
2024-11-14 17:12:43 +0800

    Add middleware and blacklist model exports to interface index for improved module accessibility

--- interface/index.ts
+++ interface/index.ts
@@ -3,3 +3,5 @@ export * from "./schema";
 export * from "./image";
 export * from "./models/user";
 export * from "./models/test";
+export * from "./middleware";
+export * from "./models/blacklist"; 
