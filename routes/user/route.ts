a2901d9dcf9df0aa03554006747e3db3b325916b
Jay200123 <kenken.ayonon@gmail.com>
2024-11-14 10:46:17 +0800

    Add user routes for CRUD operations

---
+++ routes/user/route.ts
@@ -0,0 +1,35 @@
+import * as userController from './controller';
+import { Router } from "./../../interface";
+import { METHOD, PATH, } from '../../constants';
+
+const router = Router();
+
+const userRouter = [
+    {
+        method: METHOD.GET,
+        path: PATH.USERS,
+        handler: userController.getAllUser
+    },
+    {
+        method: METHOD.GET,
+        path: PATH.USER_ID,
+        handler: userController.getUserById
+    },
+    {
+        method: METHOD.PATCH,
+        path: PATH.EDIT_USER_ID,
+        handler: userController.updateUserById
+    },
+    {
+        method: METHOD.DELETE,
+        path: PATH.USER_ID,
+        handler: userController.deleteUserById
+    }
+];
+
+userRouter.forEach((route) => {
+    const { method, path, handler } = route;
+    router[method as any](path, handler);
+});
+
+export default router;  
