5ed68c9091a88058ebf37d0a5bbba3bf2f2e8c7c
Jay200123 <kenken.ayonon@gmail.com>
2024-11-18 17:29:49 +0800

    Add image upload functionality to branch controller methods

--- routes/branch/controller.ts
+++ routes/branch/controller.ts
@@ -1,6 +1,8 @@
-import { ErrorHandler, SuccessHandler } from "../../utils";
+import { ErrorHandler, SuccessHandler, upload } from "../../utils";
 import branchService from "./service";
 import { Request, Response, NextFunction } from "../../interface";
+import { uploadImage } from "../../utils";
+import { cloudinary } from "../../config";
 
 const getAllBranch = async (req: Request, res: Response, next: NextFunction) => {
     const data = await branchService.getAll();
@@ -17,17 +19,42 @@ const getBranchById = async (req: Request, res: Response, next: NextFunction) =>
 }
 
 const createBranch = async (req: Request, res: Response, next: NextFunction) => {
-    const data = await branchService.Add(req.body);
+    const image = await uploadImage(req.files as Express.Multer.File[], []);
+
+    const data = await branchService.Add({
+        ...req.body,
+        image: image
+    });
     return SuccessHandler(res, 'Branch created successfully', data)
 }
 
 const updateBranchById = async (req: Request, res: Response, next: NextFunction) => {
-    const data = await branchService.updateById(req.params.id, req.body);
+    const branch = await branchService.getById(req.params.id);
+
+    const oldImage = Array.isArray(branch?.image)
+        ? branch.image.map((i) => i?.public_id)
+        : [];
+
+    const image = await uploadImage(req.files as Express.Multer.File[], oldImage);
+
+    const data = await branchService.updateById(req.params.id,
+        {
+            ...req.body,
+            image: image
+        });
     return SuccessHandler(res, 'Branch updated successfully', data)
 }
 
 const deleteBranchById = async (req: Request, res: Response, next: NextFunction) => {
+    const branch = await branchService.getById(req.params.id);
+
+    const branchImage = Array.isArray(branch?.image)
+        ? branch.image.map((i) => i?.public_id)
+        : [];
+
     const data = await branchService.deleteById(req.params.id);
+    await cloudinary.api.delete_resources(branchImage);
+    
     return SuccessHandler(res, 'Branch deleted successfully', data)
 }
 
