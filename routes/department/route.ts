116b883ed9bd90f702e73b2a93983f14b72a60b6
Jay200123 <kenken.ayonon@gmail.com>
2024-11-18 20:11:28 +0800

    Add department routes with CRUD operations and middleware

---
+++ routes/department/route.ts
@@ -0,0 +1,51 @@
+import { Router, Route } from "../../interface";
+import * as departmentController from "./controller";
+import { METHOD, PATH, ROLE } from "../../constants";
+import { verifyToken, userRole } from "../../middleware";
+
+const router = Router();
+
+const departmentRoutes: Route[] = [
+    {
+        method: METHOD.GET as keyof Router,
+        path: PATH.DEPARTMENTS,
+        middleware: [verifyToken],
+        role: [ROLE.ADMIN],
+        handler: departmentController.getAllDeparments
+    },
+    {
+        method: METHOD.GET as keyof Router,
+        path: PATH.DEPARTMENT_ID,
+        middleware: [verifyToken],
+        role: [ROLE.ADMIN],
+        handler: departmentController.getDepartmentById
+    },
+    {
+        method: METHOD.POST as keyof Router,
+        path: PATH.DEPARTMENTS,
+        middleware: [verifyToken],
+        role: [ROLE.ADMIN],
+        handler: departmentController.createDepartment,
+    },
+    {
+        method: METHOD.PATCH as keyof Router,
+        path: PATH.EDIT_DEPARTMENT_ID,
+        middleware: [verifyToken],
+        role: [ROLE.ADMIN],
+        handler: departmentController.updateDepartmentById
+    },
+    {
+        method: METHOD.DELETE as keyof Router,
+        path: PATH.DEPARTMENT_ID,
+        middleware: [verifyToken],
+        role: [ROLE.ADMIN],
+        handler: departmentController.deleteDepartmentById
+    }
+];
+
+departmentRoutes.forEach((route) => {
+    const { method, path, middleware = [], role = [], handler } = route;
+    router[method as any](path, middleware.concat(userRole(...role)), handler);
+});
+
+export default router;
