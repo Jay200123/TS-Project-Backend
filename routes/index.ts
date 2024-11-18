31128c080c7745dc55d0e4f1135b6da80ce2f951
Jay200123 <kenken.ayonon@gmail.com>
2024-11-18 17:13:32 +0800

    Add branch route to the main routes index

--- routes/index.ts
+++ routes/index.ts
@@ -1,3 +1,4 @@
 export { default as test } from './test/route';
 export { default as user } from './user/route';
 export { default as auth } from './authentication/route';
+export { default as branch } from './branch/route'; 
