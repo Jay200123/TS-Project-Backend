42b1f73b3601a37c4f18e2798b116c5f1bd2495e
Jay200123 <kenken.ayonon@gmail.com>
2024-11-18 20:11:02 +0800

    Add department model export to interface index

--- interface/index.ts
+++ interface/index.ts
@@ -5,4 +5,5 @@ export * from "./models/user";
 export * from "./models/test";
 export * from "./middleware";
 export * from "./models/blacklist";
-export * from "./models/branch";     
+export * from "./models/branch"; 
+export * from "./models/department";        
