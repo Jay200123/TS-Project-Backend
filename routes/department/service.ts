c0644c1e3d30dc21b3f937d42ba12b7108b559db
Jay200123 <kenken.ayonon@gmail.com>
2024-11-19 21:32:47 +0800

    Enhance Department service to populate branch data and apply collation for queries

--- routes/department/service.ts
+++ routes/department/service.ts
@@ -2,11 +2,19 @@ import Department from "./model";
 import { IDepartment } from "../../interface";
 
 const getAll = async () => {
-    return await Department.find();
+    return await Department.find()
+        .populate('branch', 'branch_name')
+        .collation({ locale: 'en' })
+        .lean()
+        .exec()
 }
 
 const getById = async (id: string) => {
-    return await Department.findById(id);
+    return await Department.findById(id)
+        .populate('branch', 'branch_name')
+        .collation({ locale: 'en' })
+        .lean()
+        .exec();
 }
 
 const Add = async (data: IDepartment) => {
