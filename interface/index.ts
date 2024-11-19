88e60b88c8bdc3082e08886f8c0bcc5ed0599ec1
Jay200123 <kenken.ayonon@gmail.com>
2024-11-19 16:52:25 +0800

    Add device model export to interface index

--- interface/index.ts
+++ interface/index.ts
@@ -5,5 +5,6 @@ export * from "./models/user";
 export * from "./models/test";
 export * from "./middleware";
 export * from "./models/blacklist";
-export * from "./models/branch"; 
-export * from "./models/department";        
+export * from "./models/branch";
+export * from "./models/department";
+export * from "./models/device";     
