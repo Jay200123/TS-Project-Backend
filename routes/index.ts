c50008e62b71984c0dc2992986b35b7b9d85c6b6
Jay200123 <kenken.ayonon@gmail.com>
2024-11-19 16:52:10 +0800

    Add device route to main routing index

--- routes/index.ts
+++ routes/index.ts
@@ -2,4 +2,6 @@ export { default as test } from './test/route';
 export { default as user } from './user/route';
 export { default as auth } from './authentication/route';
 export { default as branch } from './branch/route'; 
-export { default as department } from './department/route'; 
+export { default as department } from './department/route';
+export  { default as device } from './device/route';    
+ 
