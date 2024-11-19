73bf9d654755730cd918aff48a3286f199240e65
Jay200123 <kenken.ayonon@gmail.com>
2024-11-19 21:31:56 +0800

    Add owner and department fields to IDevice interface

--- interface/models/device.ts
+++ interface/models/device.ts
@@ -1,9 +1,11 @@
-import { Document } from "../schema";
+import { Document, mongoose } from "../schema";
 import { Image } from "../image";
 
 type Type = "printer" | "computer" | "laptop" | "monitor" | "mobile";
 
 interface IDevice extends Document {
+    owner: mongoose.Types.ObjectId,
+    department: mongoose.Types.ObjectId,
     device_name: string,
     type: Type,
     description: string,
