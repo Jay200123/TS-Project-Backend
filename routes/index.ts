9bce85144d4c283809d1fc670d96c75fb5ed5dca
Jay200123 <kenken.ayonon@gmail.com>
2024-11-19 21:32:22 +0800

    Add position route to main index file

--- routes/index.ts
+++ routes/index.ts
@@ -4,4 +4,5 @@ export { default as auth } from './authentication/route';
 export { default as branch } from './branch/route'; 
 export { default as department } from './department/route';
 export  { default as device } from './device/route';    
+export { default as position } from './position/route'; 
  
