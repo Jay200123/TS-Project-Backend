16e0c1cfc0ce7337d74c78d1214f040a0a79f97e
Jay200123 <kenken.ayonon@gmail.com>
2024-11-21 15:43:32 +0800

    Add ITicket interface for ticket management model

---
+++ interface/models/ticket.ts
@@ -0,0 +1,22 @@
+import { mongoose } from "../schema";
+import { Image } from "../image";
+
+type STATUS = "pending" | "resolved" | "in-progress" | "closed";
+type Category = "hardware" | "software" | "network";
+type LEVEL = "urgent" | "priority" | "non-urgent";
+
+interface ITicket {
+    device: mongoose.Schema.Types.ObjectId;
+    user: mongoose.Schema.Types.ObjectId;
+    description: string,
+    date_submitted: Date,
+    date_resolved: Date,
+    status: STATUS,
+    category: Category,
+    level: LEVEL,
+    assignee: mongoose.Schema.Types.ObjectId,
+    findings: string,
+    image: Image[],
+}
+
+export type { ITicket };
