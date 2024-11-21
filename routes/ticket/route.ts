bc3806653fcbaa4a95b850b79230d1a3d7419a3b
Jay200123 <kenken.ayonon@gmail.com>
2024-11-21 15:44:13 +0800

    Add ticket routes for CRUD operations with role-based access control

---
+++ routes/ticket/route.ts
@@ -0,0 +1,51 @@
+import { Router, Route } from "../../interface";
+import * as ticketController from "./controller";
+import { PATH, METHOD, ROLE } from "../../constants";
+import { verifyToken, userRole } from "../../middleware";
+
+const router = Router();
+
+const ticketRoutes: Route[] = [
+    {
+        method: METHOD.GET as keyof Router,
+        path: PATH.TICKETS,
+        middleware: [verifyToken],
+        role: [ROLE.ADMIN],
+        handler: ticketController.getAllTickets,
+    },
+    {
+        method: METHOD.GET as keyof Router,
+        path: PATH.TICKET_ID,
+        middleware: [verifyToken],
+        role: [ROLE.ADMIN],
+        handler: ticketController.getOneTicket,
+    },
+    {
+        method: METHOD.POST as keyof Router,
+        path: PATH.TICKETS,
+        middleware: [verifyToken],
+        role: [ROLE.ADMIN],
+        handler: ticketController.createTicket,
+    },
+    {
+        method: METHOD.PATCH as keyof Router,
+        path: PATH.EDIT_TICKET_ID,
+        middleware: [verifyToken],
+        role: [ROLE.ADMIN],
+        handler: ticketController.updateTicketById,
+    },
+    {
+        method: METHOD.DELETE as keyof Router,
+        path: PATH.TICKET_ID,
+        middleware: [verifyToken],
+        role: [ROLE.ADMIN],
+        handler: ticketController.deleteTicketById,
+    }
+];
+
+ticketRoutes.forEach((route) => {
+    const { method, path, middleware = [], role = [], handler } = route;
+    router[method as any](path, middleware.concat(userRole(...role)), handler);
+});
+
+export default router;
