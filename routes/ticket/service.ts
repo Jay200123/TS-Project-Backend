8ba911e06c43cfb2fdec21bd5500b4fb8cf3af9d
Jay200123 <kenken.ayonon@gmail.com>
2024-11-21 15:44:20 +0800

    Add ticket service with CRUD operations for managing tickets

---
+++ routes/ticket/service.ts
@@ -0,0 +1,58 @@
+import Ticket from "./model";
+import { ITicket } from "../../interface";
+
+const getAll = async () => {
+    return await Ticket.find()
+        .populate({
+            path: 'user',
+            select: 'fname lname branch department position',
+            populate: {
+                path: 'department',
+                select: 'department_name branch',
+                populate: {
+                    path: 'branch',
+                    select: 'branch_name'
+                }
+            },
+        })
+        .lean()
+        .exec();
+}
+
+const getById = async (id: string) => {
+    return await Ticket.findById(id)
+        .populate({
+            path: 'user',
+            select: 'fname lname branch department position',
+            populate: {
+                path: 'department',
+                select: 'department_name branch',
+                populate: {
+                    path: 'branch',
+                    select: 'branch_name'
+                }
+            },
+        })
+        .lean()
+        .exec();
+}
+
+const Add = async (data: ITicket) => {
+    return await Ticket.create(data);
+}
+
+const updateById = async (id: string, data: ITicket) => {
+    return await Ticket.findByIdAndUpdate(id, data, { new: true, runValidators: true })
+}
+
+const deleteById = async (id: string) => {
+    return await Ticket.findByIdAndDelete(id);
+}
+
+export default {
+    getAll,
+    getById,
+    Add,
+    updateById,
+    deleteById
+}
