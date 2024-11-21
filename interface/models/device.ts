bb971e5f725bb6327c656f22ee75c8f8aa36fb67
Jay200123 <kenken.ayonon@gmail.com>
2024-11-21 13:08:19 +0800

    Enhance Device model to include additional device types and new fields: date_requested, date_purchased, sales_invoice, and serial_number

--- interface/models/device.ts
+++ interface/models/device.ts
@@ -1,14 +1,29 @@
 import { Document, mongoose } from "../schema";
 import { Image } from "../image";
 
-type Type = "printer" | "computer" | "laptop" | "monitor" | "mobile";
+type Type =
+    "Printer"
+    | "System Unit"
+    | "Laptop"
+    | "Monitor"
+    | "Mobile"
+    | "AVR"
+    | "UPS"
+    | "Router"
+    | "Switch"
+    | "Hub"
+    | "Access Point"
+    | "Cable"
 
 interface IDevice extends Document {
     owner: mongoose.Types.ObjectId,
     department: mongoose.Types.ObjectId,
-    device_name: string,
     type: Type,
-    description: string,
+    description: String,
+    date_requested: Date,
+    date_purchased: Date,
+    sales_invoice: Number,
+    serial_number: String,
     image: Image[]
 };
 
