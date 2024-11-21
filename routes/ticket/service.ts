4ff13296bc866a2254d250f6f525c4787d4d5d0a
Jay200123 <kenken.ayonon@gmail.com>
2024-11-21 21:28:03 +0800

    Refactor ticket service to enhance device owner data population with detailed user information

--- routes/ticket/service.ts
+++ routes/ticket/service.ts
@@ -4,24 +4,27 @@ import { ITicket } from "../../interface";
 const getAll = async () => {
     return await Ticket.find()
         .populate({
-            path: 'user',
-            select: 'fname lname department position',
-            populate: [
-                {
-                    path: 'department',
-                    select: 'department_name branch',
-                    populate: {
-                        path: 'branch',
-                        select: 'branch_name'
+            path: "device",
+            select: "type description owner",
+            populate: {
+                path: "owner",
+                select: "fname lname branch department position",
+                populate: [
+                    {
+                        path: "branch",
+                        select: "branch_name"
+                    },
+                    {
+                        path: "department",
+                        select: "department_name"
+                    },
+                    {
+                        path: "position",
+                        select: "position_name"
                     }
-                },
-                {
-                    path: 'position',
-                    select: 'position_name'
-                }
-            ]
+                ]
+            }
         })
-        .populate("device", "type description")
         .populate("assignee", "fname lname")
         .lean()
         .exec();
@@ -30,24 +33,27 @@ const getAll = async () => {
 const getById = async (id: string) => {
     return await Ticket.findById(id)
         .populate({
-            path: 'user',
-            select: 'fname lname department position',
-            populate: [
-                {
-                    path: 'department',
-                    select: 'department_name branch',
-                    populate: {
-                        path: 'branch',
-                        select: 'branch_name'
+            path: "device",
+            select: "type description owner",
+            populate: {
+                path: "owner",
+                select: "fname lname branch department position",
+                populate: [
+                    {
+                        path: "branch",
+                        select: "branch_name"
+                    },
+                    {
+                        path: "department",
+                        select: "department_name"
+                    },
+                    {
+                        path: "position",
+                        select: "position_name"
                     }
-                },
-                {
-                    path: 'position',
-                    select: 'position_name'
-                }
-            ]
+                ]
+            }
         })
-        .populate("device", "type description")
         .populate("assignee", "fname lname")
         .lean()
         .exec();
