a7f64dccd626fc4da1d743956224aea52038d432
Jay200123 <kenken.ayonon@gmail.com>
2024-11-21 15:43:47 +0800

    Add ticket route export to main routes index

--- routes/index.ts
+++ routes/index.ts
@@ -1,8 +1,9 @@
 export { default as test } from './test/route';
 export { default as user } from './user/route';
 export { default as auth } from './authentication/route';
-export { default as branch } from './branch/route'; 
+export { default as branch } from './branch/route';
 export { default as department } from './department/route';
-export  { default as device } from './device/route';    
-export { default as position } from './position/route'; 
- 
+export { default as device } from './device/route';
+export { default as position } from './position/route';
+export { default as ticket } from './ticket/route';
+
