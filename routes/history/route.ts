f5f17858c133c7b5f7d278aaca1031ab598c97eb
Jay200123 <kenken.ayonon@gmail.com>
2024-11-22 17:08:07 +0800

    Add history routes with CRUD operations and role-based access control

---
+++ routes/history/route.ts
@@ -0,0 +1,51 @@
+import { Router, Route } from "../../interface";
+import * as historyController from "./controller";
+import { METHOD, PATH, ROLE } from "../../constants";
+import { verifyToken, userRole } from "../../middleware";
+
+const router = Router();
+
+const departmentRoutes: Route[] = [
+    {
+        method: METHOD.GET as keyof Router,
+        path: PATH.HISTORIES,
+        middleware: [],
+        role: [],
+        handler: historyController.getAllHistories
+    },
+    {
+        method: METHOD.GET as keyof Router,
+        path: PATH.HISTORY_ID,
+        middleware: [verifyToken],
+        role: [ROLE.ADMIN],
+        handler: historyController.getHistoryById
+    },
+    {
+        method: METHOD.POST as keyof Router,
+        path: PATH.HISTORIES,
+        middleware: [verifyToken],
+        role: [ROLE.ADMIN],
+        handler: historyController.createHistory,
+    },
+    {
+        method: METHOD.PATCH as keyof Router,
+        path: PATH.EDIT_HISTORY_ID,
+        middleware: [verifyToken],
+        role: [ROLE.ADMIN],
+        handler: historyController.updateHistoryById
+    },
+    {
+        method: METHOD.DELETE as keyof Router,
+        path: PATH.HISTORY_ID,
+        middleware: [verifyToken],
+        role: [ROLE.ADMIN],
+        handler: historyController.deleteHistoryById    
+    }
+];
+
+departmentRoutes.forEach((route) => {
+    const { method, path, middleware = [], role = [], handler } = route;
+    router[method as any](path, middleware.concat(userRole(...role)), handler);
+});
+
+export default router;
