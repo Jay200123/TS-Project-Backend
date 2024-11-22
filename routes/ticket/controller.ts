31546603387850ec5818cd059a40be9b812c7e98
Jay200123 <kenken.ayonon@gmail.com>
2024-11-22 17:08:18 +0800

    Integrate history service into ticket controller for device status tracking and history logging

--- routes/ticket/controller.ts
+++ routes/ticket/controller.ts
@@ -2,69 +2,106 @@ import ticketService from "./service";
 import { Request, Response, NextFunction } from "../../interface";
 import { ErrorHandler, SuccessHandler, uploadImage } from "../../utils";
 import { cloudinary } from "../../config";
+import historyService from "../history/service";
+import deviceService from "../device/service";
+import mongoose, { ObjectId } from "mongoose";
 
-const getAllTickets = async (req: Request, res: Response, next: NextFunction) => {
-    const data = await ticketService.getAll();
-    return !data || data.length === 0
-        ? next(new ErrorHandler("No Tickets Found"))
-        : SuccessHandler(res, "Tickets data found", data);
+const getAllTickets = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const data = await ticketService.getAll();
+  return !data || data.length === 0
+    ? next(new ErrorHandler("No Tickets Found"))
+    : SuccessHandler(res, "Tickets data found", data);
 };
 
-const getOneTicket = async (req: Request, res: Response, next: NextFunction) => {
-    const data = await ticketService.getById(req.params.id);
-    return !data
-        ? next(new ErrorHandler("No Ticket record found"))
-        : SuccessHandler(res, "Ticket Record found", data)
+const getOneTicket = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const data = await ticketService.getById(req.params.id);
+
+  return !data
+    ? next(new ErrorHandler("No Ticket record found"))
+    : SuccessHandler(res, "Ticket Record found", data);
 };
 
-const createTicket = async (req: Request, res: Response, next: NextFunction) => {
-    const image = await uploadImage(req.files as Express.Multer.File[], []);
-    const data = await ticketService.Add({
-        ...req.body,
-        image: image
+const createTicket = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const image = await uploadImage(req.files as Express.Multer.File[], []);
+  const data = await ticketService.Add({
+    ...req.body,
+    image: image,
+  });
+  return SuccessHandler(res, "Ticket created successfully", data);
+};
+
+const updateTicketById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const ticket = await ticketService.getById(req.params.id);
+
+  const oldImage = Array.isArray(ticket?.image)
+    ? ticket.image.map((i) => i?.public_id)
+    : [];
+
+  const deviceId = ticket.device._id.toString();
+
+  await deviceService.updateById(deviceId, {
+    status: req.body.device_status,
+  });
+
+  if (req.body.status === "resolved") {
+    const test = await historyService.Add({
+      ticket: ticket?._id,
+      device_status: req.body.device_status,
     });
-    return SuccessHandler(res, "Ticket created successfully", data);
-}
+  }
 
-const updateTicketById = async (req: Request, res: Response, next: NextFunction) => {
-    const ticket = await ticketService.getById(req.params.id);
+  const image = await uploadImage(req.files as Express.Multer.File[], oldImage);
 
-    const oldImage = Array.isArray(ticket?.image)
-        ? ticket.image.map((i) => i?.public_id)
-        : [];
+  const data = await ticketService.updateById(req.params.id, {
+    ...req.body,
+    image: image,
+  });
 
-    const image = await uploadImage(req.files as Express.Multer.File[], oldImage);
+  return SuccessHandler(res, "Ticket updated successfully", data);
+};
 
-    const data = await ticketService.updateById(req.params.id, {
-        ...req.body,
-        image: image
-    });
+const deleteTicketById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const ticket = await ticketService.getById(req.params.id);
 
-    return SuccessHandler(res, "Ticket updated successfully", data);
-}
+  const ticketImage = Array.isArray(ticket?.image)
+    ? ticket.image.map((i) => i?.public_id)
+    : [];
 
-const deleteTicketById = async (req: Request, res: Response, next: NextFunction) => {
-    const ticket = await ticketService.getById(req.params.id);
+  if (ticketImage.length > 0) {
+    await cloudinary.api.delete_resources(ticketImage);
+  }
 
-    const ticketImage = Array.isArray(ticket?.image)
-        ? ticket.image.map((i) => i?.public_id)
-        : [];
+  const data = await ticketService.deleteById(req.params.id);
 
-    if (ticketImage.length > 0) {
-        await cloudinary.api.delete_resources(ticketImage);
-    }
-
-    const data = await ticketService.deleteById(req.params.id);
-
-    return !data
-        ? next(new ErrorHandler("No Ticket record found"))
-        : SuccessHandler(res, "Ticket deleted successfully", data);
-}
+  return !data
+    ? next(new ErrorHandler("No Ticket record found"))
+    : SuccessHandler(res, "Ticket deleted successfully", data);
+};
 
 export {
-    getAllTickets,
-    getOneTicket,
-    createTicket,
-    updateTicketById,
-    deleteTicketById
-}
+  getAllTickets,
+  getOneTicket,
+  createTicket,
+  updateTicketById,
+  deleteTicketById,
+};
