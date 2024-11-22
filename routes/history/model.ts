78990926e8822a46a86a22829cca346db45f6f1f
Jay200123 <kenken.ayonon@gmail.com>
2024-11-22 17:08:02 +0800

    Update history model to use mongoose.Schema.Types and enforce required field for ticket reference

--- routes/history/model.ts
+++ routes/history/model.ts
@@ -1,11 +1,12 @@
-import { Schema, model } from "../../interface";
+import { Schema, model, mongoose } from "../../interface";
 import { IHistory } from "../../interface/";
 import { RESOURCE } from "../../constants";
 
 const historySchema: Schema<IHistory> = new Schema({
     ticket: {
-        type: Schema.Types.ObjectId,
-        ref: RESOURCE.TICKET
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: RESOURCE.TICKET,
     },
     device_status: {
         type: String,
