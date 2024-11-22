1618e1b80f33c49ae0c939882449d40abde89958
Jay200123 <kenken.ayonon@gmail.com>
2024-11-22 17:07:04 +0800

    Update ITicket interface to allow device as Types.ObjectId or IDevice

--- interface/models/ticket.ts
+++ interface/models/ticket.ts
@@ -1,12 +1,13 @@
-import { mongoose } from "../schema";
+import { mongoose, Types } from "../schema";
 import { Image } from "../image";
+import { IDevice } from "./device";
 
 type STATUS = "pending" | "resolved" | "in-progress" | "closed";
 type Category = "hardware" | "software" | "network";
 type LEVEL = "urgent" | "priority" | "non-urgent";
 
 interface ITicket {
-    device: mongoose.Schema.Types.ObjectId;
+    device: Types.ObjectId | IDevice ;
     description: string,
     date_submitted: Date,
     date_resolved: Date,
