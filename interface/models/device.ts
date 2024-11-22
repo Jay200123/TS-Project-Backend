04ff1cc0e8aa60e3d66d869863283a8117564f48
Jay200123 <kenken.ayonon@gmail.com>
2024-11-22 17:06:47 +0800

    Refactor IDevice interface to support IUser and IDepartment types for owner and department fields

--- interface/models/device.ts
+++ interface/models/device.ts
@@ -1,33 +1,41 @@
-import { Document, mongoose } from "../schema";
+import { Document, mongoose, Types } from "../schema";
 import { Image } from "../image";
+import { IUser } from "./user";
+import { IDepartment } from "./department";
 
 type Type =
-    "Printer"
-    | "System Unit"
-    | "Laptop"
-    | "Monitor"
-    | "Mobile"
-    | "AVR"
-    | "UPS"
-    | "Router"
-    | "Switch"
-    | "Hub"
-    | "Access Point"
-    | "Cable"
+  | "Printer"
+  | "System Unit"
+  | "Laptop"
+  | "Monitor"
+  | "Mobile"
+  | "AVR"
+  | "UPS"
+  | "Router"
+  | "Switch"
+  | "Hub"
+  | "Access Point"
+  | "Cable";
 
-type STATUS = "Available" | "Use" | "Repair" | "Replacement"| "Disposal" | "Return";
+type STATUS =
+  | "Available"
+  | "Use"
+  | "Repair"
+  | "Replacement"
+  | "Disposal"
+  | "Return";
 
 interface IDevice extends Document {
-    owner: mongoose.Types.ObjectId,
-    department: mongoose.Types.ObjectId,
-    type: Type,
-    description: String,
-    date_requested: Date,
-    date_purchased: Date,
-    sales_invoice: Number,
-    serial_number: String,
-    status: STATUS, 
-    image: Image[]
-};
+  owner: Types.ObjectId | IUser;
+  department: Types.ObjectId | IDepartment;
+  type: Type;
+  description: String;
+  date_requested: Date;
+  date_purchased: Date;
+  sales_invoice: Number;
+  serial_number: String;
+  status: STATUS;
+  image: Image[];
+}
 
 export type { IDevice };
