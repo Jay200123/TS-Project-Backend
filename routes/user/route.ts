556b734c6693a93c93ebe03bb65ad4997fd46893
Jay200123 <kenken.ayonon@gmail.com>
2024-11-24 20:21:13 +0800

    Refactor user routes: improve formatting and add activateUser route for user activation

--- routes/user/route.ts
+++ routes/user/route.ts
@@ -1,44 +1,51 @@
-import * as userController from './controller';
+import * as userController from "./controller";
 import { Router, Route } from "./../../interface";
-import { METHOD, PATH, ROLE } from '../../constants';
-import { verifyToken, userRole } from '../../middleware';
+import { METHOD, PATH, ROLE } from "../../constants";
+import { verifyToken, userRole } from "../../middleware";
 
 const router = Router();
 
 const userRouter: Route[] = [
-    {
-        method: METHOD.GET as keyof Router,
-        path: PATH.USERS,
-        middleware: [verifyToken],
-        role: [ROLE.ADMIN, ROLE.EMPLOYEE],
-        handler: userController.getAllUser
-    },
-    {
-        method: METHOD.GET as keyof Router,
-        path: PATH.USER_ID,
-        middleware: [verifyToken],
-        role: [ROLE.ADMIN, ROLE.EMPLOYEE],
-        handler: userController.getUserById
-    },
-    {
-        method: METHOD.PATCH as keyof Router,
-        path: PATH.EDIT_USER_ID,
-        middleware: [verifyToken],
-        role: [ROLE.ADMIN, ROLE.EMPLOYEE],
-        handler: userController.updateUserById
-    },
-    {
-        method: METHOD.DELETE as keyof Router,
-        path: PATH.USER_ID,
-        middleware: [verifyToken],
-        role: [ROLE.ADMIN],
-        handler: userController.deleteUserById
-    }
+  {
+    method: METHOD.GET as keyof Router,
+    path: PATH.USERS,
+    middleware: [verifyToken],
+    role: [ROLE.ADMIN, ROLE.EMPLOYEE],
+    handler: userController.getAllUser,
+  },
+  {
+    method: METHOD.GET as keyof Router,
+    path: PATH.USER_ID,
+    middleware: [verifyToken],
+    role: [ROLE.ADMIN, ROLE.EMPLOYEE],
+    handler: userController.getUserById,
+  },
+  {
+    method: METHOD.PATCH as keyof Router,
+    path: PATH.EDIT_USER_ID,
+    middleware: [verifyToken],
+    role: [ROLE.ADMIN, ROLE.EMPLOYEE],
+    handler: userController.updateUserById,
+  },
+  {
+    method: METHOD.DELETE as keyof Router,
+    path: PATH.USER_ID,
+    middleware: [verifyToken],
+    role: [ROLE.ADMIN],
+    handler: userController.deleteUserById,
+  },
+  {
+    method: METHOD.PATCH as keyof Router,
+    path: PATH.ACTIVATE_USER,
+    middleware: [verifyToken],
+    role: [ROLE.ADMIN],
+    handler: userController.activateUser,
+  },
 ];
 
 userRouter.forEach((route) => {
-    const { method, path, middleware = [], role = [], handler } = route;
-    router[method as any](path, middleware.concat(userRole(...role)), handler);
+  const { method, path, middleware = [], role = [], handler } = route;
+  router[method as any](path, middleware.concat(userRole(...role)), handler);
 });
 
-export default router;  
+export default router;
