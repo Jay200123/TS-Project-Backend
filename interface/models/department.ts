e720bcea9a25304e6106993a892c92c959384ff2
Jay200123 <kenken.ayonon@gmail.com>
2024-11-27 18:41:33 +0800

    Fix formatting in IDepartment interface: adjust spacing for branch property

--- interface/models/department.ts
+++ interface/models/department.ts
@@ -2,7 +2,7 @@ import { Types } from "../schema";
 import { IBranch } from "./branch";
 
 interface IDepartment {
-    branch: Types.ObjectId | IBranch; 
+    branch: Types.ObjectId | IBranch    ; 
     department_name: string;
     description: string;
 };
