9cf26cafd8dab07427799411d8f68c157d09ac1d
Jay200123 <kenken.ayonon@gmail.com>
2024-12-09 21:33:41 +0800

    Refactor IUser interface to update user properties and remove unused fields

--- interface/models/user.ts
+++ interface/models/user.ts
@@ -1,4 +1,3 @@
-import { Image } from "../image";
 import { Document, Types } from "../schema";
 import { IBranch } from "./branch";
 import { IDepartment } from "./department";
@@ -7,19 +6,16 @@ import { IPosition } from "./position";
 type Roles = "Admin" | "Technician" | "Employee";
 
 interface IUser extends Document {
-  fname: string;
-  lname: string;
+  idnumber: string; 
+  fullname: string;
   phone: string;
-  address: string;
-  city: string;
   email: string;
   password: string;
   role: Roles;
   branch: Types.ObjectId | IBranch;
   department: Types.ObjectId | IDepartment;
   position: Types.ObjectId | IPosition;
-  isAuthorized: boolean;
-  image: Image[];
+  isPasswordChanged: boolean; 
 }
 
 export type { IUser };
