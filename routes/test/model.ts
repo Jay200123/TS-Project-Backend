27f1e68c2cb92ea805eca1965a9234bca89fad76
Jay200123 <kenken.ayonon@gmail.com>
2024-12-11 16:26:12 +0800

    Add ticketNumber and counter properties to TestSchema; include createdAt field with default value

--- routes/test/model.ts
+++ routes/test/model.ts
@@ -6,13 +6,25 @@ const TestSchema: Schema<ITest> = new Schema({
         type: String,
         required: true,
     },
+    ticketNumber: {
+        type: String,
+        required: true,
+    },
+    counter: {
+        type: Number,
+        required: true, 
+    },
     image: [
         {
             public_id: String,
             url: String,
             originalname: String
         }
-    ]
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now,  
+    }
 });
 
 const Test = model<ITest>(RESOURCE.TESTS, TestSchema);
