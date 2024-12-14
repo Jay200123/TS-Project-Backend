60e263b64065071fc2a533edfa4f05058ba9be7c
Jay200123 <kenken.ayonon@gmail.com>
2024-12-14 21:49:57 +0800

    Extend ITicket interface to inherit from Document

--- interface/models/ticket.ts
+++ interface/models/ticket.ts
@@ -6,7 +6,7 @@ type STATUS = "new" | "pending" | "resolved" | "in-progress" | "closed";
 type Category = "hardware" | "software" | "network";
 type LEVEL = "urgent" | "priority" | "non-urgent";
 
-interface ITicket {
+interface ITicket extends Document {
     device: Types.ObjectId | IDevice ;
     description: string,
     date_submitted: Date,
