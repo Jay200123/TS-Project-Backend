cd9a402117f9bd00b3352fff774474e5dbab38c3
Jay200123 <kenken.ayonon@gmail.com>
2024-12-14 21:50:28 +0800

    Add equipment model with schema definition and image handling

---
+++ routes/equipment/model.ts
@@ -0,0 +1,32 @@
+import { IEquipment, Schema, model } from "../../interface";
+import { RESOURCE } from "../../constants";
+
+const equipmentSchema: Schema<IEquipment> = new Schema({
+  type: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+
+  borrowedQuantity: {
+    type: Number,
+    default: 0,
+  },
+  image: [
+    {
+      public_id: String,
+      url: String,
+      originalname: String,
+    },
+  ],
+});
+
+const Equipment = model<IEquipment>(RESOURCE.EQUIPMENT, equipmentSchema);
+export default Equipment;
