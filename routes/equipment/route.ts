7aa55b7676e9f21cb4c3fd8794dd05d996cbb605
Jay200123 <kenken.ayonon@gmail.com>
2024-12-14 21:50:35 +0800

    Add equipment routes with CRUD operations and middleware for authentication

---
+++ routes/equipment/route.ts
@@ -0,0 +1,51 @@
+import { Router, Route } from "../../interface";
+import * as equipmentController from "./controller";
+import { verifyToken, userRole } from "../../middleware";
+import { METHOD, ROLE, PATH } from "../../constants";
+
+const router = Router();
+
+const equipmentRoutes: Route[] = [
+  {
+    method: METHOD.GET as keyof Router,
+    path: PATH.EQUIPMENTS,
+    role: [ROLE.ADMIN],
+    middleware: [verifyToken],
+    handler: equipmentController.getAllEquipments,
+  },
+  {
+    method: METHOD.GET as keyof Router,
+    path: PATH.EQUIPMENT_ID,
+    role: [ROLE.ADMIN],
+    middleware: [verifyToken],
+    handler: equipmentController.getOneEquipment,
+  },
+  {
+    method: METHOD.POST as keyof Router,
+    path: PATH.EQUIPMENTS,
+    role: [ROLE.ADMIN],
+    middleware: [verifyToken],
+    handler: equipmentController.createEquipment,
+  },
+  {
+    method: METHOD.PATCH as keyof Router,
+    path: PATH.EDIT_EQUIPMENT_ID,
+    role: [ROLE.ADMIN],
+    middleware: [verifyToken],
+    handler: equipmentController.updateEquipmentById,
+  },
+  {
+    method: METHOD.DELETE as keyof Router,
+    path: PATH.EQUIPMENT_ID,
+    role: [ROLE.ADMIN],
+    middleware: [verifyToken],
+    handler: equipmentController.deleteEquipmentById,
+  },
+];
+
+equipmentRoutes.forEach((router) => {
+  const { method, path, role = [], middleware = [], handler } = router;
+  router[method](path, middleware.concat(userRole(...role)), handler);
+});
+
+export default router;
