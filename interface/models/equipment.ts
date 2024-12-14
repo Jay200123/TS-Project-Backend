71a9ce5fbf3016725bc0c23ec51efddfb914b384
Jay200123 <kenken.ayonon@gmail.com>
2024-12-14 21:49:30 +0800

    Add IEquipment interface to define equipment model structure

---
+++ interface/models/equipment.ts
@@ -0,0 +1,13 @@
+import { Document } from "../schema";
+import { Image } from "../image";
+
+interface IEquipment extends Document {    
+    type: string;
+    brand: string;
+    price: number;
+    quantity: number; 
+    borrowedQuantity: number;  
+    image: Image[];
+}
+
+export type { IEquipment };
