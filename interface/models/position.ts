32062353b50298d8f13f786fe81d387e2e4bef97
Jay200123 <kenken.ayonon@gmail.com>
2024-12-14 21:49:48 +0800

    Extend IPosition interface to inherit from Document

--- interface/models/position.ts
+++ interface/models/position.ts
@@ -1,7 +1,7 @@
 import { Types } from "../schema";
 import { IDepartment } from "./department";
 
-interface IPosition {
+interface IPosition extends Document {
     department: Types.ObjectId | IDepartment;
     position_name: string;
     description: string;
