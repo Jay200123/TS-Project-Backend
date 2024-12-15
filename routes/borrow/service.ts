0b3d39214365c84dfc3363497a993e4d00d18469
Jay200123 <kenken.ayonon@gmail.com>
2024-12-15 15:26:20 +0800

    Add borrow service with CRUD operations for borrowing functionality

---
+++ routes/borrow/service.ts
@@ -0,0 +1,77 @@
+import Borow from "./model";
+import { IBorrow } from "../../interface";
+
+const getAll = async () => {
+  return await Borow.find()
+    .populate({
+      path: "user",
+      select: "fullname position",
+      populate: {
+        path: "position",
+        select: "position_name department",
+        populate: {
+          path: "department",
+          select: "department_name",
+        },
+      },
+    })
+    .populate({
+      path: "lender",
+      select: "fullname position",
+      populate: {
+        path: "position",
+        select: "position_name",
+      },
+    })
+    .lean()
+    .exec();
+};
+
+const getById = async (id: string) => {
+  return await Borow.findById(id)
+    .populate({
+      path: "user",
+      select: "fullname position",
+      populate: {
+        path: "position",
+        select: "position_name department",
+        populate: {
+          path: "department",
+          select: "department_name",
+        },
+      },
+    })
+    .populate({
+      path: "lender",
+      select: "fullname position",
+      populate: {
+        path: "position",
+        select: "position_name",
+      },
+    })
+    .lean()
+    .exec();
+};
+
+const Add = async (data: IBorrow) => {
+  return await Borow.create(data);
+};
+
+const updateById = async (id: string, data: Partial<IBorrow>) => {
+  return await Borow.findByIdAndUpdate(id, data, {
+    runValidators: true,
+    new: true,
+  });
+};
+
+const deleteById = async (id: string) => {
+  return await Borow.findByIdAndDelete(id);
+};
+
+export default {
+  getAll,
+  getById,
+  Add,
+  updateById,
+  deleteById,
+};
