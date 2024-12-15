129a8d519a91f1b080812271030e6d53e868cbed
Jay200123 <kenken.ayonon@gmail.com>
2024-12-15 15:25:51 +0800

    Add borrow route export to main routes index

--- routes/index.ts
+++ routes/index.ts
@@ -1,12 +1,11 @@
-export { default as test } from './test/route';
-export { default as user } from './user/route';
-export { default as auth } from './authentication/route';
-export { default as branch } from './branch/route';
-export { default as department } from './department/route';
-export { default as device } from './device/route';
-export { default as position } from './position/route';
-export { default as ticket } from './ticket/route';
-export { default as history } from './history/route';
-export { default as equipment } from './equipment/route';
-
-
+export { default as test } from "./test/route";
+export { default as user } from "./user/route";
+export { default as auth } from "./authentication/route";
+export { default as branch } from "./branch/route";
+export { default as department } from "./department/route";
+export { default as device } from "./device/route";
+export { default as position } from "./position/route";
+export { default as ticket } from "./ticket/route";
+export { default as history } from "./history/route";
+export { default as equipment } from "./equipment/route";
+export { default as borrow } from "./borrow/route";
