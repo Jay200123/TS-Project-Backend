b9646ead57011246ad0e74bf5fdffa51f86690b3
Jay200123 <kenken.ayonon@gmail.com>
2024-12-19 16:28:11 +0800

    Add frontend origin to allowed origins list

--- config/allowedOrigins.ts
+++ config/allowedOrigins.ts
@@ -1,6 +1,7 @@
  const allowedOrigins = [
     "http://localhost:5173",
     "https://ts-project-backend.vercel.app",
+    "https://ts-project-frontend-b8yv.vercel.app",
 ]
 
 export default allowedOrigins;
